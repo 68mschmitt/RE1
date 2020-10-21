32b02c9c2bee94cc8de9e5bf9e565018f5111fb7
68mschmitt <68mschmitt@gmail.com>
2020-10-20 23:28:09 -0400

    Initital commit

---
+++ Website/Controllers/StudyController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Website.Models;
+
+namespace Website.Controllers
+{
+    public class StudyController : Controller
+    {
+        public IActionResult Index()
+        {
+            var cards = new List<Card>() {
+                new Card {
+                ID = 1,
+                Front = "Yo soy",
+                Back = "I am"
+                }, new Card {
+                ID = 2,
+                Front = "Yo estoy",
+                Back = "I am currently"
+                }, new Card {
+                ID = 3,
+                Front = "Yo estoy",
+                Back = "I am currently"
+                }
+            };
+
+            return View(cards);
+        }
+    }
+}
---
+++ Website/Models/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Website.Models
+{
+    public class Card
+    {
+        public int ID { get; set; }
+        public string Front { get; set; }
+        public string Back { get; set; }
+    }
+}
